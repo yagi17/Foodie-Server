e81df212aa5bd74550491c0b355d4a3d1ca4a663
yagi17 <nafissadik785@gmail.com>
2024-05-11 10:22:42 +0600

    delete item by id

--- index.js
+++ index.js
@@ -74,6 +74,13 @@ app.get('/allMenu/:id', async (req, res) => {
     res.send(result);
 });
 
+// delete item by id
+app.delete('/allMenu/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await userCollection.deleteOne(query);
+    res.send(result);
+});
 
 
 app.get('/', async (req, res) => {
