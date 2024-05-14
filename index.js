0f96be966849566bfb80326ec34e1344f9c51dbe
yagi17 <nafissadik785@gmail.com>
2024-05-14 23:40:03 +0600

    set the origins

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://foodie-9960c.web.app/'
+        'https://foodie-9960c.web.app',
     ],
     credentials: true
 }));
